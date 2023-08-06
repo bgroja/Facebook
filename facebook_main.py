436d7771c764d9b13d5638717c2b92502c90c508
Ghattigeppagol <roja.ghattigeppagol@capgemini.com>
2023-08-06 10:58:02 +0530

    first commit

---
+++ facebook_main.py
@@ -0,0 +1,54 @@
+import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import test_facebookc
+from test_facebook import TestNewFacebook
+from selenium.webdriver.chrome.service import Service
+from flask import Flask, render_template
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.keys import Keys
+app = Flask(__name__)
+
+service = Service("C:\Program Files\browsers\chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, service=service)
+
+
+@app.route('/')
+def run_testcases():
+    return render_template("facebook.html")
+
+
+@app.route('/run_testcases', methods=['POST'])
+def test_case_results():
+    Validationresult = test_Url()
+    return render_template("facebook.html", Validationresult=Validationresult)
+def test_Url():
+    driver.get("https://www.facebook.com")
+    element = TestNewFacebook.test_launch_facebook('self')
+    element.click()
+    Validationresult = 'Pass'
+    return Validationresult
+
+
+def launch_webaddress():
+    webbrowser.open('http://127.0.0.1:5001/')
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
+    launch_webaddress()
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_facebook.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from flask import Flask, render_template
+import pytest
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+service = Service("C:\\Program Files\\browsers\\chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, service=service)
+
+
+class TestNewFacebook:
+
+  def test_launch_facebook(self):
+      driver.get('https://www.facebook.com/')
+      wait = WebDriverWait(driver, 5)
+      create_account_element = wait.until(EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/div[1]/div[1]/div/div/div/div[1]/div/img')))
+      att = create_account_element.get_attribute('src')
+      if att == 'https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg':
+       Validationresult = 'Pass'
+      else:
+       Validationresult = 'Fail'
+      element = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a')
+      element.click()
+      print(Validationresult)
+      return create_account_element
+
+
+#run_launch_facebook = TestNewFacebook.test_launch_facebook('self')
